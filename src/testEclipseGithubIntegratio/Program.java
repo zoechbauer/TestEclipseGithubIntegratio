7a06acc2fba3fbb0b6cfa819c9bab3650d340d51
Zoechbauer <hans.zoechbauer@aon.at>
2020-07-29 18:26:53 +0200

    another test of spaces instead of tabs

--- src/testEclipseGithubIntegratio/Program.java
+++ src/testEclipseGithubIntegratio/Program.java
@@ -1,42 +1,63 @@
 package testEclipseGithubIntegratio;
 
 public class Program {
-	// intendation code formatting changed
-	// spaces insted of tabs
-	// tab = 3 spaces
+   // intendation code formatting changed
+   // spaces insted of tabs
+   // tab = 3 spaces
 
-	public static void main(String[] args) {
-		testIntendationInGithub();
+   public static void main(String[] args) {
+      testIntendationInGithub();
+      testIntendationInGithub2();
 
-	}
+   }
 
-	public static void testIntendationInGithub() {
-		anotherMethod();
-		if (true) {
-			andOneMore();
-			if (false) {
-				lastMethod();
-			} else {
-				for (int i = 0; i < 10; i++) {
-					System.out.println(i);
-				}
-			}
-		}
+   public static void testIntendationInGithub() {
+      anotherMethod();
+      if (true) {
+         andOneMore();
+         if (false) {
+            lastMethod();
+         } else {
+            for (int i = 0; i < 10; i++) {
+               System.out.println(i);
+            }
+         }
+      }
 
-	}
+   }
 
-	public static void anotherMethod() {
-		System.out.println("anotherMethod");
+   public static void main(String[] args) {
+      testIntendationInGithub();
 
-	}
+   }
 
-	public static void andOneMore() {
-		System.out.println("andOneMore");
+   public static void testIntendationInGithub2() {
+      anotherMethod();
+      if (true) {
+         andOneMore();
+         if (false) {
+            lastMethod();
+         } else {
+            for (int i = 0; i < 10; i++) {
+               System.out.println(i);
+            }
+         }
+      }
 
-	}
+   }
 
-	public static void lastMethod() {
-		System.out.println("lastMethod");
-	}
+   public static void anotherMethod() {
+      System.out.println("anotherMethod");
+
+   }
+
+   public static void andOneMore() {
+      System.out.println("andOneMore");
+
+   }
+
+   public static void lastMethod() {
+      System.out.println("lastMethod");
+   }
 
 }
